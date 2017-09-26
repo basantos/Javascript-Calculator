cc584cdef47285d90457f1cd1e99f440fa49f023
basantos <santosbrigitte@gmail.com>
2017-09-25 18:13:02 -0700

    Add calculating mechanism

--- script.js
+++ script.js
@@ -13,15 +13,66 @@ function multiply(x,y){
 }
 
 function divide(x,y){
-  return x/y;
+  return x.toFixed(1)/y.toFixed(1);
 }
 
-var displayHasOperator = false;
+/* Variables */
+var enteredButtons = [];
+var lastAnswerOnScreen = false;
+var operators = /[\+x\/\-]/;
+
+/* Event Listeners */
 $('.display-enabled-value').click(function(){
-  var displayText= $('#display').text() + $(this).data('value');
+  var displayText;
+  // Don't run if first input is an operator
+  if(enteredButtons.length === 0 && operators.test($(this).data('value'))){
+    displayText = '';
+  } // Continue operations when equels-button already hit
+  else if(lastAnswerOnScreen && operators.test($(this).data('value'))){
+    displayText = $('#display').text() + $(this).data('value');
+    enteredButtons.push($(this).data('value'));
+    lastAnswerOnScreen = false;
+  } // Deletes last session when new number is pressed
+  else if(lastAnswerOnScreen && !operators.test($(this).data('value'))){
+    displayText = $(this).data('value');
+    lastAnswerOnScreen = false;
+    enteredButtons = [];
+    enteredButtons.push($(this).data('value'));
+  } // Add up the operations as normal
+  else {
+    displayText = $('#display').text() + $(this).data('value');
+    enteredButtons.push($(this).data('value'));
+  }
+
   $('#display').text(displayText);
 });
 
 $('#ac').click(function(){
   $('#display').text('');
+  lastAnswerOnScreen = false;
+  enteredButtons = [];
 })
+
+$('#equals-button').click(function(){
+  var answer = enteredButtons[0];
+  for(var i=1; i<enteredButtons.length; i++){
+    if(i%2 !== 0){
+      if(enteredButtons[i] === '+'){
+        answer = add(answer,enteredButtons[i+1]);
+      }
+      else if(enteredButtons[i] === '-'){
+        answer = subtract(answer,enteredButtons[i+1]);
+      }
+      else if(enteredButtons[i] === 'x'){
+        answer = multiply(answer,enteredButtons[i+1]);
+      }
+      else if(enteredButtons[i] === '/'){
+        answer = divide(answer,enteredButtons[i+1]);
+      }
+    }
+  }
+
+  enteredButtons = [answer];
+  lastAnswerOnScreen = true;
+  $('#display').text(answer);
+});
