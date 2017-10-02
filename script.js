40457c4a74e8b2cbcd4f1fb1967e9a51ca3aa020
basantos <santosbrigitte@gmail.com>
2017-10-01 21:19:17 -0700

    Fix floating point numbers and Infinity

--- script.js
+++ script.js
@@ -1,32 +1,39 @@
-const SCREEN_LIMIT = 15;
+const SCREEN_LIMIT = 14;
 
 /* Operations */
 function add(x,y){
-  return formatNum(x*1+y*1);
+  return x*1+y*1;
 }
 
 function subtract(x,y){
-  return formatNum(x*1-y*1);
+  return x*1-y*1;
 }
 
 function multiply(x,y){
-  return formatNum((x*1)*(y*1));
+  debugger;
+  return (x*1)*(y*1);
 }
 
 function divide(x,y){
-  return formatNum((x*1).toFixed(1)/(y*1).toFixed(1));
+  return (x*1).toFixed(15)/(y*1).toFixed(15);
 }
 
-function formatNum(num){
-  num = num.toFixed(15).toString().split('');
-  for(var i=num.length-1; i>0; i--){
-    if(num[i] === '0' || num[i] === '.'){
-      num.pop();
-    } else {
-      return num.join('');
-    }
-  }
-}
+// function formatNum(num){
+//   debugger;
+//   num = num.toFixed(15).toString().split('');
+//   for(var i=num.length-1; i>0; i--){
+//     if(num[i] === '0'){
+//       num.pop();
+//     } else {
+//       // Take off decimal when number is an integer
+//       if(num[num.length-1]==='.'){
+//         num.pop();
+//       }
+//
+//       return num.join('')*1;
+//     }
+//   }
+// }
 
 /* Variables */
 var displayText = '';
@@ -39,6 +46,7 @@ var errorOnScreen = false;
 
 /* Event Listeners */
 $('.number-button').click(function(){
+  debugger;
 
   if(errorOnScreen){
     enteredButtons = [];
@@ -50,7 +58,7 @@ $('.number-button').click(function(){
     errorOnScreen = false;
   }
 
-  if(SCREEN_LIMIT <= displayText.toString().trim().length){
+  if(SCREEN_LIMIT < displayText.toString().trim().length){
     displayText = 'Limit reached.';
     errorOnScreen = true;
   } // Deletes last session when new number is pressed
@@ -70,7 +78,15 @@ $('.number-button').click(function(){
 });
 
 $('.operator-button').click(function(){
-  // Don't run if first input is an operator
+  // if(errorOnScreen){
+  //   enteredButtons = [];
+  //   nextNum = '';
+  //   lastAnswerOnScreen = true;
+  //   alreadyDecimal = false;
+  //   previousIsOperator = false;
+  //   displayText = '';
+  //   errorOnScreen = false;
+  // } // Don't run if first input is an operator
   if(nextNum === '' && enteredButtons.length === 0){
     displayText = '';
   } // Makes sure there's only one operator per pair of numbers
@@ -79,6 +95,15 @@ $('.operator-button').click(function(){
     enteredButtons.pop();
     enteredButtons.push($(this).data('value'));
     displayText = displayText.slice(0,displayText.length-1) + $(this).data('value');
+  } // Continue operations when equals-button already pressed at least once in a session
+  // when last answer on screen as long as the screen limit
+  else if(lastAnswerOnScreen && displayText.length >= SCREEN_LIMIT){
+    displayText = $(this).data('value');
+    enteredButtons.push($(this).data('value'));
+    nextNum = '';
+    lastAnswerOnScreen = false;
+    alreadyDecimal = false;
+    previousIsOperator = true;
   } // Continue operations when equals-button already pressed at least once in a session
   else if(lastAnswerOnScreen){
     displayText = $('#display').text() + $(this).data('value');
@@ -100,6 +125,7 @@ $('.operator-button').click(function(){
 });
 
 $('#decimal-button').click(function(){
+  debugger;
   if(errorOnScreen){
     enteredButtons = [];
     nextNum = '';
@@ -111,16 +137,22 @@ $('#decimal-button').click(function(){
   }
 
   if(lastAnswerOnScreen){
+    nextNum = '.';
     lastAnswerOnScreen = false;
     alreadyDecimal = true;
-    nextNum = '.';
+    displayText = '.';
     enteredButtons = [];
-    $('#display').text($(this).data('value'));
-  } else if(!alreadyDecimal){
+  } else if(alreadyDecimal === false && nextNum === ''){
+    alreadyDecimal = true;
+    nextNum = $(this).data('value');
+    displayText = $('#display').text() + $(this).data('value');
+  } else if(alreadyDecimal === false){
     alreadyDecimal = true;
     nextNum += $(this).data('value');
-    $('#display').text($('#display').text() + $(this).data('value'));
+    displayText = $('#display').text() + $(this).data('value');
   }
+
+  $('#display').text(displayText);
 });
 
 $('#ac').click(function(){
@@ -132,27 +164,34 @@ $('#ac').click(function(){
 });
 
 $('#equals-button').click(function(){
+  debugger;
   var answer = enteredButtons[0];
-  for(var i=1; i<enteredButtons.length; i++){
+  var initialEnteredButtonsLength = enteredButtons.length;
+  for(var i=1; i<initialEnteredButtonsLength; i++){
     if(i%2 !== 0){
-      if(enteredButtons[i] === '+'){
+      // add last number in input last
+      if(i===initialEnteredButtonsLength-1){
         enteredButtons.push(nextNum);
+      }
+
+      if(enteredButtons[i] === '+'){
         answer = add(answer,enteredButtons[i+1]);
       }
       else if(enteredButtons[i] === '-'){
-        enteredButtons.push(nextNum);
         answer = subtract(answer,enteredButtons[i+1]);
       }
       else if(enteredButtons[i] === '*'){
-        enteredButtons.push(nextNum);
         answer = multiply(answer,enteredButtons[i+1]);
       }
       else if(enteredButtons[i] === '/'){
-        enteredButtons.push(nextNum);
         answer = divide(answer,enteredButtons[i+1]);
       }
     }
   }
+  if(SCREEN_LIMIT < answer.toString().trim().length){
+    answer = (answer*1).toPrecision(SCREEN_LIMIT-1);
+  }
+
   alreadyDecimal = false;
   previousIsOperator = false;
   displayText = answer;
